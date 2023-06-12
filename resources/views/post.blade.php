fef0326b93e19f8f718e495e5f544c9bcda6bd66
= <=>
2023-06-12 11:52:36 -0600

    First commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/app.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>My Blog</title></head>
+
+<body>
+
+    <article>
+        
+    <?= $post; ?>
+
+    <a href="/">Go Back</a>
+
+</body>
+</html>
+
+
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/app.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>My Blog</title></head>
+
+<body>
+
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p align="justify">
+        Lorem ipsum dolor sit amet consectetur adipiscing elit fermentum, praesent accumsan nunc facilisis iaculis inceptos tortor felis vitae, gravida elementum mus donec tempus per neque. Ante pharetra non posuere ut dapibus nisl condimentum dignissim vulputate, purus laoreet sagittis velit pellentesque donec dictum odio, parturient duis risus potenti accumsan semper tincidunt suspendisse. Ridiculus blandit nunc ullamcorper integer interdum malesuada lectus neque massa, vehicula mollis tristique dapibus pulvinar maecenas non aliquet libero, in sodales cum consequat tellus aptent lacus ultricies. Eu iaculis ultrices potenti tempus nibh leo aptent lacus congue nisl lectus natoque laoreet, scelerisque vivamus fringilla dictumst integer massa quisque facilisi vulputate fermentum fusce.
+        </p>
+
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p align="justify">
+        Lorem ipsum dolor sit amet consectetur adipiscing elit fermentum, praesent accumsan nunc facilisis iaculis inceptos tortor felis vitae, gravida elementum mus donec tempus per neque. Ante pharetra non posuere ut dapibus nisl condimentum dignissim vulputate, purus laoreet sagittis velit pellentesque donec dictum odio, parturient duis risus potenti accumsan semper tincidunt suspendisse. Ridiculus blandit nunc ullamcorper integer interdum malesuada lectus neque massa, vehicula mollis tristique dapibus pulvinar maecenas non aliquet libero, in sodales cum consequat tellus aptent lacus ultricies. Eu iaculis ultrices potenti tempus nibh leo aptent lacus congue nisl lectus natoque laoreet, scelerisque vivamus fringilla dictumst integer massa quisque facilisi vulputate fermentum fusce.
+        </p>
+
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p align="justify">
+        Lorem ipsum dolor sit amet consectetur adipiscing elit fermentum, praesent accumsan nunc facilisis iaculis inceptos tortor felis vitae, gravida elementum mus donec tempus per neque. Ante pharetra non posuere ut dapibus nisl condimentum dignissim vulputate, purus laoreet sagittis velit pellentesque donec dictum odio, parturient duis risus potenti accumsan semper tincidunt suspendisse. Ridiculus blandit nunc ullamcorper integer interdum malesuada lectus neque massa, vehicula mollis tristique dapibus pulvinar maecenas non aliquet libero, in sodales cum consequat tellus aptent lacus ultricies. Eu iaculis ultrices potenti tempus nibh leo aptent lacus congue nisl lectus natoque laoreet, scelerisque vivamus fringilla dictumst integer massa quisque facilisi vulputate fermentum fusce.
+        </p>
+
+    </article>
+
+</body>
+</html>
+
+
+
+
